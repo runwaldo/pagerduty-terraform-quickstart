9dcc59d8741ce8613575c0d4a1a563b5fa642c33
Oswaldo <oswaldo@pagerduty.com>
2025-12-04 08:06:47 -0500

    Update to Terraform 1.14.1 and PagerDuty provider 3.30.7

--- terraform.tf
+++ terraform.tf
@@ -1,9 +1,9 @@
 terraform {
-  required_version = ">= 0.13.0"
+  required_version = ">= 1.14.1"
   required_providers {
     pagerduty = {
       source  = "pagerduty/pagerduty"
-      version = "2.14.5"
+      version = "~> 3.30"
     }
   }
 }
